ff29e4739bacb0ddd94c75daf7a5931473a01ed4
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-12 18:41:12 +0530

    added

--- flowlog.tf
+++ flowlog.tf
@@ -1,5 +1,5 @@
 resource "aws_flow_log" "vpcflowlogs" {
-  log_destination      = aws_s3_bucket.mys3-bucket.arn
+  log_destination      = aws_s3_bucket.mybucket.arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.myvpc.id
