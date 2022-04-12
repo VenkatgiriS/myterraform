c9756377d6dd757bdf0432f4088fa4d167f84e58
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-12 23:09:26 +0530

    added 3ec2 and sg

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@
      availability_zone = "us-east-1a"
      instance_type = "t2.nano"
      key_name = "devops_project"
-     subnet_id = "${aws_subnet.mysubnet3.id}"
+     subnet_id = "${aws_subnet.mysubnet1.id}"
      vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
      associate_public_ip_address = true	
      tags = {
@@ -22,7 +22,7 @@
      availability_zone = "us-east-1a"
      instance_type = "t2.nano"
      key_name = "devops_project"
-     subnet_id = "${aws_subnet.mysubnet3.id}"
+     subnet_id = "${aws_subnet.mysubnet1.id}"
      vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
      associate_public_ip_address = true	
      tags = {
@@ -39,7 +39,7 @@
      availability_zone = "us-east-1a"
      instance_type = "t2.nano"
      key_name = "devops_project"
-     subnet_id = "${aws_subnet.mysubnet3.id}"
+     subnet_id = "${aws_subnet.mysubnet1.id}"
      vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
      associate_public_ip_address = true	
      tags = {
