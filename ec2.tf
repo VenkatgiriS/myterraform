d2feb9dd70086422ca4142ae169f75cf51668092
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-14 00:49:42 +0530

    added subnet-id to ec2 using spalsh syntax

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,18 @@
-#  resource "aws_instance" "web-1" {
-#      count =3 #0,1,2
-#      #ami = var.imagename
-#      ami = "ami-04505e74c0741db8d"
-#      #ami = "${data.aws_ami.my_ami.id}"
-#      availability_zone = "us-east-1a"
-#      instance_type = "t2.nano"
-#      key_name = "devops_project"
-#      subnet_id = "${aws_subnet.mysubnet1.id}"
-#      vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#      associate_public_ip_address = true	
-#      tags = {
-#          Name = "Server-${count.index + 1 }"
-#          Env = "Prod"
-#          Owner = "Venkat"
-#  	CostCenter = "ABCD"
-#      }
-#  }
+ resource "aws_instance" "web-1" {
+     count =3 #0,1,2
+     #ami = var.imagename
+     ami = "ami-04505e74c0741db8d"
+     #ami = "${data.aws_ami.my_ami.id}"
+     #availability_zone = "us-east-1a"
+     instance_type = "t2.nano"
+     key_name = "devops_project"
+     subnet_id = "${element(aws_subnet.public-subnets.*.id , count.index)}"
+     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Server-${count.index + 1 }"
+         Env = "Prod"
+         Owner = "Venkat"
+ 	CostCenter = "ABCD"
+     }
+ }
