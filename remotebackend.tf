0de1922b3ff7d60b44753e0295e8be225448ab0c
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-13 22:59:25 +0530

    added

--- remotebackend.tf
+++ remotebackend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-bucket.com"
     key    = "myterraform.tfstate"
-    dynamodb_table = "terraform-lock"
+    #dynamodb_table = "terraform-lock"
     region = "us-east-1"
   }
 }
