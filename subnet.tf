e1b8ebf32d2747685ad2fd94d072689de4f33868
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-13 23:59:48 +0530

    modified

--- subnet.tf
+++ subnet.tf
@@ -2,8 +2,8 @@ resource "aws_subnet" "public-subnets" {
   count = 3
   vpc_id     = aws_vpc.myvpc.id
   #cidr_block = "${var.subnet1_cidr}"
-  cidr_block = "${element(var.public_subnets, count.index)}"
-  availability_zone = "${element(var.public_cidrs, count.index)}"
+  availability_zone = "${element(var.public_subnets, count.index)}"
+  cidr_block = "${element(var.public_cidrs, count.index)}"
   tags = {
     Name = "public-subnet-${count.index+1}"
     Owner = "Venkat"
