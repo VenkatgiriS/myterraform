fbf69a1801af7e00800855ed037afba32e4b0910
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-15 00:22:22 +0530

    edited security groups

--- sg.tf
+++ sg.tf
@@ -4,16 +4,19 @@ resource "aws_security_group" "allow_all" {
   vpc_id      = "${aws_vpc.myvpc.id}"
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    }
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
