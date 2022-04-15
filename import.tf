b191e957b4617172f66b9a0b33dd09e5e60c0456
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-15 23:29:04 +0530

    after import

--- import.tf
+++ import.tf
@@ -1,18 +1,16 @@
  resource "aws_instance" "web-10" {
-    #  count = "${var.env == "Prod"}"? 2 : 1 #0,1,2
-    #  #ami = var.imagename
-    #  ami = "${lookup(var.amis,var.region)}"
-    #  #ami = "${data.aws_ami.my_ami.id}"
-    #  #availability_zone = "us-east-1a"
-    #  instance_type = "t2.nano"
-    #  key_name = "devops_project"
-    #  subnet_id = "${element(aws_subnet.public-subnets.*.id , count.index)}"
-    #  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-    #  associate_public_ip_address = true	
-    #  tags = {
-    #      Name = "Server-${count.index + 1 }"
-    #      Env = "${var.env}"
-    #      Owner = "Venkat"
- 	# CostCenter = "ABCD"
-    #  }
+     #count = "${var.env == "Prod"}"? 2 : 1 #0,1,2
+     ami = ami-03ededff12e34e59e
+     #ami = "${lookup(var.amis,var.region)}"
+     #ami = "${data.aws_ami.my_ami.id}"
+     #availability_zone = "us-east-1a"
+     instance_type = "t2.micro"
+     key_name = "devops_project"
+     subnet_id = "subnet-040a95ea95f4661b9"
+     vpc_security_group_ids = "sg-0b22a8d1d6161e774"
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Import"
+ 
+     }
  }
