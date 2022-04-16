fac8aa32227e8e849edd879ec4d112e87d10a772
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-16 18:03:53 +0530

    debugging provisioner

--- ec2.tf
+++ ec2.tf
@@ -33,12 +33,13 @@ resource "aws_instance" "web-1" {
   provisioner "file" {
     source      = "script.sh"
     destination = "/tmp/script.sh"
-  }
+  
   connection {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("devops_project.pem")
     host = self.public_ip
     #host        = element(aws_instance.web-1.*.public_ip, count.index)
+  }
   }
 }
