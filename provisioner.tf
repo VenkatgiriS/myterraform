dcaef61f67c2d3536bc61f764250a760a03f098c
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-16 19:39:01 +0530

    null resource

--- provisioner.tf
+++ provisioner.tf
@@ -1,14 +1,15 @@
-  resource "null_resource" "add_file" {
-    provisioner "file" {
-      source      = "script.sh"
-      destination = "/tmp/script.sh"
-      connection {
-        type        = "ssh"
-        user        = "ubuntu"
-        private_key = file("devops_project.pem")
-        host = aws_instance.web-1.public_ip
-        #host        = self.public_ip
-        #host        = element(aws_instance.web-1.*.public_ip, count.index)
-      }
+resource "null_resource" "add_file" {
+  count = "${var.env == "Prod"}" ? 2 : 1
+  provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("devops_project.pem")
+      #host = aws_instance.web-1.public_ip
+      #host        = self.public_ip
+      host = element(aws_instance.web-1.*.public_ip, count.index)
     }
   }
+}
