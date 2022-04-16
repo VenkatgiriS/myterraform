46e6d2bcadcbc615d080fddccd24d353fe784bbb
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-16 18:33:17 +0530

    modified ingress in sg

--- sg.tf
+++ sg.tf
@@ -3,13 +3,11 @@ resource "aws_security_group" "allow_all" {
   description = "Allow all inbound traffic"
   vpc_id      = "${aws_vpc.myvpc.id}"
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.myvpc.cidr_block]
-    #ipv6_cidr_blocks = [aws_vpc.myvpc.ipv6_cidr_block]
+  iingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
