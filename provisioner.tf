026af52e3637d8def7851874334bcfd945ff1621
VenkatgiriS <venkatgiri877@gmail.com>
2022-04-16 20:34:05 +0530

    modified

--- provisioner.tf
+++ provisioner.tf
@@ -29,7 +29,7 @@ resource "null_resource" "nginxinstall" {
       "sudo apt install cowsay -y",
       "cowsay 'Hi Venkatgiri'",
       "sudo service nginx start",
-      "echo '<h1>Server-${count.index+1}</h1>' | sudo tee /var/www/html/index.nginx-debian.html"
+      "echo '<h1>Hi Venkatgiri.This is Server-${count.index+1}</h1>' | sudo tee /var/www/html/index.nginx-debian.html"
 
     ]
     connection {
